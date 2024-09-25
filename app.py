de7849c22aa2a0924dd77d60fb3f94cf953770f4
Joey, K <40598758+naeyanika@users.noreply.github.com>
2024-09-25 10:12:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,37 +6,39 @@ def muat_data_dari_github(url):
     data = pd.read_excel(url)
     return data
 
+# Link to raw GitHub content
+url_github = 'https://github.com/naeyanika/rcm/raw/master/RCM.xlsx'
 
-#Link Github
-url_github = 'https://github.com/naeyanika/rcm/blob/master/RCM.xlsx'
+# Load Data
+try:
+    data = muat_data_dari_github(url_github)
+    st.title('Pencarian Data Risk Control Matrix')
 
-#Muat Data
-data = muat_data_dari_github(url_github)
+    # Input user kata kunci
+    kata_kunci = st.text_input("Masukkan kata kunci yang ingin dicari:")
 
-st.title('Pencarian Data Risk Control Matrix')
+    # Fungsi Pencarian
+    def cari_data(data, kata_kunci):
+        hasil = data[
+            data['KC/KR/KP'].str.contains(kata_kunci, case=False, na=False) |
+            data['Judul Temuan'].str.contains(kata_kunci, case=False, na=False) |
+            data['Kode Risk Issue'].str.contains(kata_kunci, case=False, na=False) |
+            data['Judul Risk Issue'].str.contains(kata_kunci, case=False, na=False) |
+            data['Kategori'].str.contains(kata_kunci, case=False, na=False) |
+            data['Kelemahan'].str.contains(kata_kunci, case=False, na=False) 
+        ]
+        return hasil
 
-# Input user kata kunci
-kata_kunci = st.text_input("Masukkan kata kunci yang ingin dicari:")
+    # Jika ada input kata kunci, lakukan pencarian
+    if kata_kunci:
+        hasil_pencarian = cari_data(data, kata_kunci)
+        
+        # Tampilkan hasil pencarian
+        if not hasil_pencarian.empty:
+            st.write(hasil_pencarian)
+        else:
+            st.write("Tidak ada hasil yang cocok dengan kata kunci.")
 
-# Fungsi Pencarian
-def cari_data(data, kata_kunci):
-    hasil = data[
-        data['KC/KR/KP'].str.contains(kata_kunci, case=False, na=False) |
-        data['Judul Temuan'].str.contains(kata_kunci, case=False, na=False) |
-        data['Kode Risk Issue'].str.contains(kata_kunci, case=False, na=False) |
-        data['Judul Risk Issue'].str.contains(kata_kunci, case=False, na=False) |
-        data['Kategori'].str.contains(kata_kunci, case=False, na=False) |
-        data['Kelemahan'].str.contains(kata_kunci, case=False, na=False) 
-    ]
-    return hasil
-
-# Jika ada input kata kunci, lakukan pencarian
-if kata_kunci:
-    hasil_pencarian = cari_data(data, kata_kunci)
-    
-    # Tampilkan hasil pencarian
-    if not hasil_pencarian.empty:
-        st.write(hasil_pencarian)
-    else:
-        st.write("Tidak ada hasil yang cocok dengan kata kunci.")
-
+except Exception as e:
+    st.error(f"An error occurred: {str(e)}")
+    st.write("Please check if the file is accessible and the URL is correct.")
